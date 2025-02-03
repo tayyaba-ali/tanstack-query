fc1b38d15ccc3b4d8e214bb60e0c8dbb18e72bcf
iqranaz240 <iqranaz240@gmail.com>
2025-02-03 10:57:05 +0500

    need to be done add to do

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+import Todo from "./components/Todo";
+
+
+
+const App = () => {
+  return (
+    
+    <div> 
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
